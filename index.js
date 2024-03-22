726b0bf18fdc37e1fa91dd36467480718775dabc
Nelson1Aguiar <132019512+Nelson1Aguiar@users.noreply.github.com>
2024-03-22 20:07:20 -0300

    Portfolio Nelson Sales

---
+++ index.js
@@ -0,0 +1,53 @@
+const wpp=document.getElementById("wpp");
+const email=document.getElementById("email");
+const github=document.getElementById("github");
+const linkedin= document.getElementById("linkedin");
+
+const GerLanche=document.getElementById("GerLanche");
+const GerEscola=document .getElementById("GerEscola");
+const biblioteca=document.getElementById("Biblioteca");
+const todo=document.getElementById("Todo");
+const op=document.getElementById("Op");
+const calculadora=document.getElementById("Calculadora");
+
+
+GerLanche.addEventListener('click', function() {
+    window.location.href = 'https://github.com/Nelson1Aguiar/Lanchonete_C-EDA.git';
+});
+
+GerEscola.addEventListener('click', function() {
+    window.location.href = 'https://github.com/Nelson1Aguiar/Sistema_GerenciamentoDeEscola-Java-Mysql.git';
+});
+
+todo.addEventListener('click', function() {
+    window.location.href = 'https://github.com/Nelson1Aguiar/Lista_de_tarefas-React.git';
+});
+
+biblioteca.addEventListener('click', function() {
+    window.location.href = 'https://github.com/Nelson1Aguiar/Biblioteca_Online.git';
+});
+
+op.addEventListener('click', function() {
+    window.location.href = 'https://github.com/Nelson1Aguiar/Progresso-OP.git';
+});
+
+calculadora.addEventListener('click', function() {
+    window.location.href = 'https://github.com/Nelson1Aguiar/Calculadora-ReactNative.git';
+});
+
+
+wpp.addEventListener('click', function() {
+    window.location.href = 'https://wa.me/5588996829896';
+});
+
+email.addEventListener('click', function() {
+    window.location.href = 'mailto:nelsonsales20.ns@gmail.com';
+});
+
+github.addEventListener('click', function() {
+    window.location.href = 'https://github.com/Nelson1Aguiar';
+});
+
+linkedin.addEventListener('click', function() {
+    window.location.href = 'https://www.linkedin.com/in/nelson-sales-a07788249/';
+});
