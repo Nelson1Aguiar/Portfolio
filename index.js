f33299dfd3f7b5aa710075e2fbf0e211ae7fe2e7
Nelson Sales <132019512+Nelson1Aguiar@users.noreply.github.com>
2024-04-04 08:44:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,63 @@
-const wpp=document.getElementById("wpp");
-const email=document.getElementById("email");
-const github=document.getElementById("github");
-const linkedin= document.getElementById("linkedin");
+document.addEventListener('DOMContentLoaded', function() {
+    const wpp = document.getElementById("wpp");
+    const email = document.getElementById("email");
+    const github = document.getElementById("github");
+    const linkedin = document.getElementById("linkedin");
 
-const GerLanche=document.getElementById("GerLanche");
-const GerEscola=document .getElementById("GerEscola");
-const biblioteca=document.getElementById("Biblioteca");
-const todo=document.getElementById("Todo");
-const op=document.getElementById("Op");
-const calculadora=document.getElementById("Calculadora");
-const pingpong=document.getElementById("PingPong");
-const telefoneWeb = document.getElementById("TelefoneWeb");
+    const GerLanche = document.getElementById("GerLanche");
+    const GerEscola = document.getElementById("GerEscola");
+    const biblioteca = document.getElementById("Biblioteca");
+    const todo = document.getElementById("Todo");
+    const op = document.getElementById("Op");
+    const calculadora = document.getElementById("Calculadora");
+    const pingpong = document.getElementById("PingPong");
+    const telefoneWeb = document.getElementById("TelefoneWeb");
 
+    GerLanche.addEventListener('click', function() {
+        window.location.href = 'https://github.com/Nelson1Aguiar/Lanchonete_C-EDA.git';
+    });
 
-GerLanche.addEventListener('click', function() {
-    window.location.href = 'https://github.com/Nelson1Aguiar/Lanchonete_C-EDA.git';
-});
+    GerEscola.addEventListener('click', function() {
+        window.location.href = 'https://github.com/Nelson1Aguiar/Sistema_GerenciamentoDeEscola-Java-Mysql.git';
+    });
 
-GerEscola.addEventListener('click', function() {
-    window.location.href = 'https://github.com/Nelson1Aguiar/Sistema_GerenciamentoDeEscola-Java-Mysql.git';
-});
+    todo.addEventListener('click', function() {
+        window.location.href = 'https://github.com/Nelson1Aguiar/Lista_de_tarefas-React.git';
+    });
 
-todo.addEventListener('click', function() {
-    window.location.href = 'https://github.com/Nelson1Aguiar/Lista_de_tarefas-React.git';
-});
+    biblioteca.addEventListener('click', function() {
+        window.location.href = 'https://github.com/Nelson1Aguiar/Biblioteca_Online.git';
+    });
 
-biblioteca.addEventListener('click', function() {
-    window.location.href = 'https://github.com/Nelson1Aguiar/Biblioteca_Online.git';
-});
+    op.addEventListener('click', function() {
+        window.location.href = 'https://github.com/Nelson1Aguiar/Progresso-OP.git';
+    });
 
-op.addEventListener('click', function() {
-    window.location.href = 'https://github.com/Nelson1Aguiar/Progresso-OP.git';
-});
+    calculadora.addEventListener('click', function() {
+        window.location.href = 'https://github.com/Nelson1Aguiar/Calculadora-ReactNative.git';
+    });
 
-calculadora.addEventListener('click', function() {
-    window.location.href = 'https://github.com/Nelson1Aguiar/Calculadora-ReactNative.git';
-});
+    pingpong.addEventListener('click', function() {
+        window.location.href = 'https://github.com/Nelson1Aguiar/GamePingPong_javascript.git';
+    });
 
-pingpong.addEventListener('click',function(){
-    window.location.href = 'https://github.com/Nelson1Aguiar/GamePingPong_javascript.git';
-})
+    telefoneWeb.addEventListener('click', function() {
+        window.location.href = 'https://github.com/Nelson1Aguiar/TelefoneWeb.git';
+    });
 
-telefoneWeb.addEventListener('click',function(){
-    window.location.href = 'https://github.com/Nelson1Aguiar/TelefoneWeb.git';
-})
+    wpp.addEventListener('click', function() {
+        window.location.href = 'https://wa.me/5588996829896';
+    });
 
+    email.addEventListener('click', function() {
+        window.location.href = 'mailto:nelsonsales20.ns@gmail.com';
+    });
 
+    github.addEventListener('click', function() {
+        window.location.href = 'https://github.com/Nelson1Aguiar';
+    });
 
-wpp.addEventListener('click', function() {
-    window.location.href = 'https://wa.me/5588996829896';
-});
-
-email.addEventListener('click', function() {
-    window.location.href = 'mailto:nelsonsales20.ns@gmail.com';
-});
-
-github.addEventListener('click', function() {
-    window.location.href = 'https://github.com/Nelson1Aguiar';
-});
-
-linkedin.addEventListener('click', function() {
-    window.location.href = 'https://www.linkedin.com/in/nelson-sales-a07788249/';
+    linkedin.addEventListener('click', function() {
+        window.location.href = 'https://www.linkedin.com/in/nelson-sales-a07788249/';
+    });
 });
